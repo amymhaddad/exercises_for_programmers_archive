6a079535455a1060ed86e1166d4078283988d4d0
amymhaddad <39930340+amymhaddad@users.noreply.github.com>
2018-06-04 19:15:56 -0400

    exercise_one

---
+++ ex_1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  3 09:17:31 2018
+
+@author: amyhaddad
+"""
+
+user_name = "Amy" 
+print(f"Hello, {user_name}.")
+
+print(f"What is your lastname, {user_name}?")
+input()
+
+print("What is your name?")
+name = input()
+
+print(f"Your name is {name}.")
+
+prompt = '>'
+
+print("What do you like to do?")
+do = input(prompt)
+
+input("My name is")
