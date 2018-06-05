d096fef577825c4be139e06cae7794b4618150e2
amymhaddad <39930340+amymhaddad@users.noreply.github.com>
2018-06-05 16:00:10 -0400

    ex_1_v2.py

---
+++ ex_1_v2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  5 15:29:27 2018
+
+@author: amyhaddad
+"""
+
+
+name=input("What is your name?")
+print(f"Hello, {name}, nice to meet you!")
