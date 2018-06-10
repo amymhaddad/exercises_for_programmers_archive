7a73695654d265f22afaeef60d4f3158a2d3c667
amymhaddad <39930340+amymhaddad@users.noreply.github.com>
2018-06-10 08:58:39 -0400

    ex_3.py
    Please disregard ex_2.py and refer to ex_3.py

---
+++ ex_3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 10 08:52:42 2018
+
+@author: amyhaddad
+"""
+
+
+print("This is exercise 3.")
+
+print("What is your favorite quote?", end= ' ')
+quote = input()
+
+print(f"Who said \"{quote}\"? ", end = ' ')
+said = input()
+
+print("{} said the quote \"{}.\" ".format(said, quote))
