b89447d21df2066f3135d21095be8957eea7497c
Amy Haddad <amy.m.haddad@gmail.com>
2018-06-25 15:05:20 -0400

    ex2_ch1.py
    Instructions: Create a program that prompts for your name and prints a greeting using your name.
    
    Example Output:
           What is your name? Brian
           Hello, Brian, nice to meet you!

---
+++ ex2_ch1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 10 15:34:22 2018
+
+@author: amyhaddad
+"""
+
+#step1: ask for name
+user_name = input("What is your name? ")
+
+#step2: print name with greeting
+print(f"Hello, {user_name}, it is nice to meet you!")
