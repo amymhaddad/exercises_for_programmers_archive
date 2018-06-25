10041f8aa177a3e28284bb3c333c56cabfea7284
Amy Haddad <amy.m.haddad@gmail.com>
2018-06-25 15:22:10 -0400

    ex3_3_ch1.py
    Instructions: Create a program that prompts for a quote and an author. Display the quotation and author as shown in the example output.
    
    Example Output:
           What is the quote? These aren't the droids you're looking for.
           Who said it? Obi-Wan Kenobi
           Obi-Wan Kenobi says, "These aren't the droids
           you're looking for."

---
+++ ex3_3_ch1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 10 15:34:22 2018
+
+@author: amyhaddad
+"""
+
+user_favorite_quote = input("What is your favorite quote? ")
+
+quote_author = input("Who said that quote? ")
+
+print(f"{quote_author} said {user_favorite_quote}.")
