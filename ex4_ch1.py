fdc4520538321d3783135bc011c7c07f97157b62
Amy Haddad <amy.m.haddad@gmail.com>
2018-06-25 15:34:50 -0400

    ex4_ch1.py
    Instructions:  Create a simple mad-lib program that prompts for a noun, a verb, an adverb, and an adjective and injects those into a story that you create.
    
    Example Output
           Enter a noun: dog
           Enter a verb: walk
           Enter an adjective: blue
           Enter an adverb: quickly
           Do you walk your blue dog quickly? That's hilarious!

---
+++ ex4_ch1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 10 15:34:22 2018
+
+@author: amyhaddad
+"""
+
+noun = input("Enter a noun. ")
+
+verb = input("Enter a verb. ")
+
+adverb = input("Enter an adverb. ")
+
+adjective = input("Enter an adjective. ")
+
+print(f"Your {adjective} {noun} {verb} {adverb}. That's hilarious!")
