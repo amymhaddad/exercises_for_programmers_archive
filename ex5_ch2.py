1abb451178ebde07de78539ecc869d964921a6be
Amy Haddad <amy.m.haddad@gmail.com>
2018-06-26 16:06:29 -0400

    Update ex5_ch2.py

--- ex5_ch2.py
+++ ex5_ch2.py
@@ -14,22 +14,22 @@ first_number = int(input("This is your first number: "))
 #step2: write input statement asking for second number
 second_number = int(input("This is your second number: "))
 
-#step2: create variable "sum" and write an equation that adds the two numbers together and print equation and solutoin 
+#step2: create variable "sum" and write an equation that adds the two numbers together and print equation and solution 
 sum = first_number + second_number 
 
 print(f"{first_number} + {second_number} = {sum}")
 
-#step3: create variable "difference" and write an equation that subtracts the two numbers and print the equation and solutionon 
+#step3: create variable "difference" and write an equation that subtracts the two numbers and print the equation and solution 
 difference = first_number - second_number
 
 print(f"{first_number} - {second_number} = {difference}")
 
-#step4: create variable "product" and write an equation that multiplies the two numbers together; print eq and soltuion
+#step4: create variable "product" and write an equation that multiplies the two numbers together; print equation and solution
 product = first_number * second_number
 
 print(f"{first_number} * {second_number} = {product}")
 
-#step5: create variable "quotient" and write an equation that divides the two numbers; print equation and soltuoin
+#step5: create variable "quotient" and write an equation that divides the two numbers; print equation and solution
 quotient = first_number / second_number
 
 print(f"{first_number} / {second_number} = {math.ceil(quotient)}")
