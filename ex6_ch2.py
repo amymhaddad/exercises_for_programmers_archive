cca6eb74f1189d03f0fe49397c0e2fe0344368d9
Amy Haddad <amy.m.haddad@gmail.com>
2018-06-27 08:11:33 -0400

    ex6_ch2.py
    Instructions: Create a program that determines how many years you have left until retirement and the year you can retire. It should prompt for your current age and the age you want to retire and display the output as shown in the example that follows.
    
    Example Output:
           What is your current age? 25
           At what age would you like to retire? 65
           You have 40 years left until you can retire.
           It's 2015, so you can retire in 2055.

---
+++ ex6_ch2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 10 15:34:22 2018
+
+@author: amyhaddad
+"""
+
+
+
+#step1: ask user for input: age
+current_age = int(input("What is your age? "))
+
+#step2: ask user for input: ideal retirement age
+ideal_retirement_age = int(input("What age do you want to retire? "))
+
+#step3: write calculation to determine the number of years until retirement
+years_to_retirement = ideal_retirement_age - current_age
+
+#step4: write calculation to determine the year you can retire
+retirement_year = 2018 + years_to_retirement
+
+#step5: print number of years until retirement
+print(f"You have {years_to_retirement} years left until you can retire.")
+
+#step 6: print current year and the year user can retire
+print(f"It's 2018, so you can retire in {retirement_year}.")
+
