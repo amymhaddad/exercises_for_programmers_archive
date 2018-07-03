df95cbb814de9e9902749561e72975086b0cd74d
Amy Haddad <amy.m.haddad@gmail.com>
2018-07-03 16:16:14 -0400

    ex11_ch3.py
    Instructions: Write a program that converts currency. Specifically, convert euros to U.S. dollars. Prompt for the amount of money in euros you have, and prompt for the current exchange rate of the euro. Print out the new amount in U.S. dollars.
    
    Example Output:
    How many euros are you exchanging? 81
    What is the exchange rate? 137.51
    81 euros at an exchange rate of 137.51 is
    111.38 U.S. dollars.

---
+++ ex11_ch3.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 10 15:34:22 2018
+
+@author: amyhaddad
+"""
+
+#step1: get input on amount of money in euros
+amount_of_euros = float(input("How much money in euros do you have? "))
+
+#step2: get input about the exchange rate
+euro_exchange_rate = float(input("What is the current euro exchange rate? "))
+
+#step3: write calculation to convert the euros to US dollars
+dollar_exchange_rate = 1.17
+
+conversion_euros_to_dollars = (amount_of_euros * euro_exchange_rate) / dollar_exchange_rate
+rounded_conversion = round(conversion_euros_to_dollars)
+
+#step4: print a statement with number of euros, euro exchange rate and the amount in US dollars
+print(f"You have {amount_of_euros} euros at an exhange rate of {euro_exchange_rate}, which is about ${rounded_conversion} US dollars.")
