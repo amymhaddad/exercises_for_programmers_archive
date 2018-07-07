891647c75000f9b9d8111999409c5c1cff8bb129
Amy Haddad <amy.m.haddad@gmail.com>
2018-07-07 13:26:25 -0400

    Update whats_your_name.py

--- whats_your_name.py
+++ whats_your_name.py
@@ -6,7 +6,9 @@ Created on Thu May 10 15:34:22 2018
 @author: amyhaddad
 """
 
+#write two variables: one on each line
 firstname = "Amy"
 lastname = "Haddad"
 
+#print statement using variables
 print(f"Hello {firstname} {lastname}! You just delved into python.")
