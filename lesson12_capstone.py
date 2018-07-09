9d5de6e2f4adea370b0b7265e36369e63aef0b63
Amy Haddad <amy.m.haddad@gmail.com>
2018-07-09 08:44:54 -0400

    Update lesson12_capstone.py

--- lesson12_capstone.py
+++ lesson12_capstone.py
@@ -21,7 +21,7 @@ space2 = full_name_2.find(" ")
 second_firstname = full_name_2[0:space2]
 second_lastname = full_name_2[space2+1:]
 
-#step3: write a line of code to change the first letter of each name using index and .replace
+#step3: write a line of code to change the first letter of each name using index and use .replace
 first_letter_firstname_name_1 = first_firstname[0]
 first_letter_lastname_name_1 = first_lastname[0]
 
@@ -32,14 +32,14 @@ new_firstname = first_firstname.replace(first_letter_firstname_name_1, first_let
 new_lastname = first_lastname.replace(first_letter_lastname_name_1, first_letter_lastname_name_2)
 print(f"Here's the first new name option: {new_firstname} {new_lastname}")
 
-#step3: write a line of code to swap the names using a tuple
+#step4: write a line of code to swap the names using a tuple
 swap_firstname = (first_firstname, second_firstname) = (second_firstname, first_firstname)
 swap_lastname = (first_lastname, second_lastname) = (second_lastname, first_lastname)
 
-#step3a: add the truples together so all objects are within one truple
+#step4a: add the truples together so all objects are within one truple
 swapped_name = swap_firstname + swap_lastname
 
-#step3b: index the newly formed truple to form new first and last names for one name
+#step4b: index the newly formed truple to form new first and last names for one name
 swapped_new_first_name1 = swapped_name[0]
 swapped_new_last_name1 = swapped_name[3]
 swapped_first_and_lastname_v1 = (swapped_new_first_name1, swapped_new_last_name1)
@@ -47,7 +47,7 @@ swapped_first_and_lastname_v1 = (swapped_new_first_name1, swapped_new_last_name1
 swapped_new_firstname_name1 = swapped_first_and_lastname_v1[0]
 new_lastname_name1 = swapped_first_and_lastname_v1[1]
 
-#step3c: index the newly formed truple to form new first and last names for second name
+#step4c: index the newly formed truple to form new first and last names for second name
 swapped_new_first_name2 = swapped_name[1]
 swapped_new_last_name2 = swapped_name[2]
 swapped_first_and_lastname_v2 = (swapped_new_first_name2, swapped_new_last_name2)
