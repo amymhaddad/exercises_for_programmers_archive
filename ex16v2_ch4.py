ebbe564a5aef1e5d6c3a77ce45b33c7b230e5285
Amy Haddad <amy.m.haddad@gmail.com>
2018-07-20 07:59:38 -0400

    ex16v2_ch4.py
    Write a program that asks the user for their age and compare it to the legal driving age of sixteen. If the user is sixteen or older, then the program should display “You are old enough to legally drive.” If the user is under sixteen, the program should display “You are not old enough to legally drive.”

---
+++ ex16v2_ch4.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 10 15:34:22 2018
+
+@author: amyhaddad
+"""
+
+#step1: state constant
+LEGAL_DRIVING_AGE = 16
+
+#step2: get input from user
+age = int(input("Enter your age: "))
+
+#step3: set up 3 conditionals: if user provides invalid input, if 16 or older, or if younger than 16
+if age < 0:
+    print("Please enter a valid age.")
+
+elif age >= 16:
+    print("You're legally able to drive.")
+else: 
+    print("You're too young to drive legally.")
