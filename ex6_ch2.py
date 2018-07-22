5e290fa442796937e34d5629cec61b32487c8b5a
Amy Haddad <amy.m.haddad@gmail.com>
2018-07-22 08:58:29 -0400

    Update ex6_ch2.py
    Made CURRENT_YEAR a constant.

--- ex6_ch2.py
+++ ex6_ch2.py
@@ -6,6 +6,8 @@ Created on Thu May 10 15:34:22 2018
 @author: amyhaddad
 """
 
+CURRENT_YEAR = 2018
+
 #step1: ask user for input: age
 current_age = int(input("What is your age? "))
 
@@ -16,10 +18,10 @@ ideal_retirement_age = int(input("What age do you want to retire? "))
 years_to_retirement = ideal_retirement_age - current_age
 
 #step4: write calculation to determine the year you can retire
-retirement_year = 2018 + years_to_retirement
+retirement_year = CURRENT_YEAR + years_to_retirement
 
 #step5: print number of years until retirement
 print(f"You have {years_to_retirement} years left until you can retire.")
 
 #step 6: print current year and the year user can retire
-print(f"It's 2018, so you can retire in {retirement_year}.")
+print(f"It's {CURRENT_YEAR}, so you can retire in {retirement_year}.")
