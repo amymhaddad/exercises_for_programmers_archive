3b1f5380216029a108dbea7d1894ba6718919b62
Amy Haddad <amy.m.haddad@gmail.com>
2018-07-23 16:26:29 -0400

    Update ex_3.py

--- ex_3.py
+++ ex_3.py
@@ -6,13 +6,7 @@ Created on Sun Jun 10 08:52:42 2018
 @author: amyhaddad
 """
 
+quote = input("Enter your favorite quote: ")
+author = input("Who said the quote? ")
 
-print("This is exercise 3.")
-
-print("What is your favorite quote?", end= ' ')
-quote = input()
-
-print(f"Who said \"{quote}\"? ", end = ' ')
-said = input()
-
-print("{} said the quote \"{}.\" ".format(said, quote))
+print(f"{author.title()} said, \"{quote}.\" ")
