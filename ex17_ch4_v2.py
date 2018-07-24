597b86017531429bb5851c1c2839bea0e17a810d
Amy Haddad <amy.m.haddad@gmail.com>
2018-07-24 14:53:58 -0400

    Update ex17_ch4_v2.py

--- ex17_ch4_v2.py
+++ ex17_ch4_v2.py
@@ -6,16 +6,13 @@ Created on Thu May 10 15:34:22 2018
 @author: amyhaddad
 """
 
-#step1: write out constants
+#step1: write out constant
 BAC_ILLEGAL_TO_DRIVE = 0.08
 
 #step2: get input
 gender = input("Enter your gender, male or female: ")
-
 weight = int(input("Enter your weight in pounds: "))
-
 time = int(input("Enter the number of hours since your last drink. If it's been less than one hour enter 1: "))
-
 alcohol = int(input("Enter the amount of alcohol in ounces: "))
 
 #step3: set up two conditionals based on male or female response (since the ratio for the BAC equation depends on gender)
