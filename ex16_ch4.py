7ae26d304e547235be9250b937eb3e223d9ffbc2
Amy Haddad <amy.m.haddad@gmail.com>
2018-07-25 15:10:14 -0400

    Update ex16_ch4.py

--- ex16_ch4.py
+++ ex16_ch4.py
@@ -9,17 +9,14 @@ Created on Thu May 10 15:34:22 2018
 #step1: state constant
 LEGAL_DRIVING_AGE = 16
 
-#step2: get input from user on their age
-user_age = int(input("Enter your age: "))
+#step2: get input from user
+age = int(input("Enter your age: "))
 
-#step3: set up first conditional
-if user_age >= LEGAL_DRIVING_AGE:
-    print("You're old enough to drive legally.")
-
-#step4: set up second conditional if user enters a number less than 0
-if user_age < 0: 
+#step3: set up conditionals: if user provides invalid input, if 16 or older, or if younger than 16
+if age < 0:
     print("Please enter a valid age.")
 
-#step5: set up conditional if user is older than 0 but less than 16
-if (user_age > 0) and (user_age < LEGAL_DRIVING_AGE):
-    print("You're not old enough to drive legally.")
+if age >= LEGAL_DRIVING_AGE:
+    print("You're legally able to drive.")
+else: 
+    print("You're too young to drive legally.")
