c3d1cb7eccae18e988b819858bcc1b8a6d3d518a
Amy Haddad <amy.m.haddad@gmail.com>
2018-07-25 15:31:25 -0400

    Update page199_question4.py

--- page199_question4.py
+++ page199_question4.py
@@ -6,20 +6,16 @@ Created on Thu May 10 15:34:22 2018
 @author: amyhaddad
 """
 
-#step1: state number variable
-secret_number = 11
+#step1: state constant
+SECRET_NUMBER = 11
 
 #step2: get user input to guess the number
 user_guess = int(input("Guess my secret number: "))
 
-#step3: write one conditional statement re: if guess is too low
+#step3: write conditionals
 if user_guess < 11:
     print("Too low.")
-
-#step4: write second conditional statement re: if guess is too high
-if user_guess > 11:
+elif user_guess > 11:
     print("Too high.")
-
-#step5: write third conditional statement re: if guess is correct
-if user_guess == 11:
+else: 
     print("You got it!")
