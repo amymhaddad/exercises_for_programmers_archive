7f3c618089b52e6b323761b9e5901e95fa76dbd8
Amy Haddad <amy.m.haddad@gmail.com>
2018-07-25 15:04:04 -0400

    Update ex22_ch4.py

--- ex22_ch4.py
+++ ex22_ch4.py
@@ -13,11 +13,11 @@ number3 = int(input("Enter a third number: "))
 #step2: set up inequalities to make sure all numbers are different. Then, set up another conditional if the numbers are different.
 if number1 == number2 or number1 == number3 or number2 == number3:
     print("The numbers are not different. Game over.")
-elif number1 != number2 or number1 != number3 or number2 != number3:
+else:
     print("Let's continue with the game.")
     if number1 > number2 and number1 > number3:
         print(f"{number1} is the largest number.")
     elif number2 > number1 and number2 > number3:
         print(f"{number2} is the largest number.")
-    elif number3 > number2 and number3 > number1:
+    else: 
         print(f"{number3} is the largest number.")
