8f8966dfb92aed42ed3766ceb0eacfcbc1a706fa
Amy Haddad <amy.m.haddad@gmail.com>
2018-07-25 15:12:12 -0400

    Update ex16_ch4.py

--- ex16_ch4.py
+++ ex16_ch4.py
@@ -15,7 +15,6 @@ age = int(input("Enter your age: "))
 #step3: set up conditionals: if user provides invalid input, if 16 or older, or if younger than 16
 if age < 0:
     print("Please enter a valid age.")
-
 if age >= LEGAL_DRIVING_AGE:
     print("You're legally able to drive.")
 else: 
