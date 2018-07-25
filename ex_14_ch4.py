9d5500ac63361b61d3194db61764366bd25b5c1c
Amy Haddad <amy.m.haddad@gmail.com>
2018-07-25 15:18:21 -0400

    Update ex_14_ch4.py

--- ex_14_ch4.py
+++ ex_14_ch4.py
@@ -6,24 +6,20 @@ Created on Thu May 10 15:34:22 2018
 @author: amyhaddad
 """
 
-#step1: create constant
-WI_RESIDENT_TAX = 5.5
+#step1: state constant
+WI_TAX_PERCENT = 5.5
 
-#step2: get input from user for the total order amount
-order_subtotal = float(input("Enter the total order amount: $"))
+#step2: get inputs from user on order amount and state
+order_amount = float(input("Enter the order amount:$"))
+state = input("What state do you live in? Enter the state abbreviation (eg, MA): ")
+normalized_state = state.lower()
 
-#step3: get input from user on the state
-state = input("Enter the state you live in, using an abbreviation (eg, MA): ")
-
-#step4: make tax calculation for WI resident
-tax_amount = WI_RESIDENT_TAX / 100 * order_subtotal
-total_amount_with_tax = order_subtotal + tax_amount
-
-#step4: write if statement to determine if user's response is WI
-if state == "WI":
-    print("The WI sales tax is 5.5%")
-    print("Your order subtotal is ${:.2f}".format(order_subtotal)+".")
-    print("Your tax amount is ${:.2f}".format(total_amount_with_tax)+".")
-
-if state != "WI":
-    print("You're not a WI resident, so your total is ${:.2f}".format(order_subtotal)+".")
+#step3: set up first conditional to see if user lives in WI
+if normalized_state == "wi":
+    print(f"You're a Wisonsin resident, and will be charged a tax of {WI_TAX_PERCENT}%.")
+    tax_amount = WI_TAX_PERCENT / 100 * order_amount
+    total_with_tax = tax_amount + order_amount
+    print("Your order amount before tax is ${:.2f}".format(order_amount)+", but with tax your grand total is ${:.2f}.".format(total_with_tax))
+#step4: set up conditional for users who don't live in WI
+else: 
+    print("You're not a Wisconsin resident, so your total is ${:.2f}.".format(order_amount))
