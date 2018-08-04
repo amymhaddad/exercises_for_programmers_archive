4a3ad682e705ed4ed94ced6fe40be5a6fe867f66
Amy Haddad <amy.m.haddad@gmail.com>
2018-08-04 13:26:58 -0400

    Update ex13_ch3.py

--- ex13_ch3.py
+++ ex13_ch3.py
@@ -7,7 +7,7 @@ Created on Thu May 10 15:34:22 2018
 """
 
 #step1: get input: principal amount
-principal_amount = float(input("Enter the principal amount: $"))
+principal_amount = float(input("Enter the principal amount:$"))
 
 #step2: get input: interest rate -- and use float
 annual_interest_rate = float(input("Enter the annual interest rate: "))
@@ -22,7 +22,7 @@ years_invested = int(input("Enter the number of years the amount has been invest
 frequency_interest_compounded_yearly = int(input("Enter the number of times the interest is compounded each year: "))
 
 #step6: write calculation to compute the value of investment compounded over time
-investment_compounded_over_time = principal_amount *(1 + int(annual_interest_rate) / frequency_interest_compounded_yearly)**frequency_interest_compounded_yearly*years_invested
+investment_compounded_over_time = principal_amount * (1 + (annual_interest_rate) / frequency_interest_compounded_yearly) ** frequency_interest_compounded_yearly * years_invested
 
 #step7: write a statement with values for each input
 print(f"${principal_amount} invested at {annual_interest_rate}% for {years_invested} years compounded {frequency_interest_compounded_yearly} times per year is ${int(investment_compounded_over_time)}.")
