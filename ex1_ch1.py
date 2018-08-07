01b2f1827519d4fed1c32b8154445f0876995bb1
Amy Haddad <amy.m.haddad@gmail.com>
2018-08-07 16:14:31 -0400

    Update ex1_ch1.py

--- ex1_ch1.py
+++ ex1_ch1.py
@@ -13,10 +13,10 @@ bill_amount = float(input("How much was your bill? "))
 tip_percentage = int(input("How much will you tip? "))
 
 #step3: convert tip to a decimal
-tip_amount = tip_percentage/100 * bill_amount
+tip_amount = tip_percentage / 100 * bill_amount
 
 #step4: add tip to bill amount
 bill_with_tip = tip_amount + bill_amount
 
 #step5: print statement to show tip amount and bill amount
-print(f"The tip is ${int(tip_amount)} and the total bill with tip is ${int(bill_with_tip)}.")
+print(f"The tip is ${tip_amount} and the total bill with tip is ${bill_with_tip}.")
