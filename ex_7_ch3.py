ed1e4b22ce3a61e36880e2249d67b83354090bac
Amy Haddad <amy.m.haddad@gmail.com>
2018-08-07 16:03:47 -0400

    Update ex_7_ch3.py

--- ex_7_ch3.py
+++ ex_7_ch3.py
@@ -6,6 +6,8 @@ Created on Thu May 10 15:34:22 2018
 @author: amyhaddad
 """
 
+SQ_FT_TO_METERS = 0.092903
+
 length = int(input("What is the length of the room in feet? "))
 width = int(input("What is the width of the rrom in feet? "))
 
@@ -14,5 +16,5 @@ print(f"You entered dimensions of {length} feet by {width} feet.")
 square_feet = length * width
 print(f"The area of the room is {square_feet} square feet.")
 
-square_meters = square_feet * 0.092903
+square_meters = square_feet * SQ_FT_TO_METERS 
 print(f"The area of the room is {square_meters} square meters.")
