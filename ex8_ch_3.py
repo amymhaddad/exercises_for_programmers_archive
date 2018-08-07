55ff32ddfb09e8a173909dbc6ba7f442bef55769
Amy Haddad <amy.m.haddad@gmail.com>
2018-08-07 11:03:55 -0400

    Update ex8_ch_3.py

--- ex8_ch_3.py
+++ ex8_ch_3.py
@@ -6,30 +6,26 @@ Created on Thu May 10 15:34:22 2018
 @author: amyhaddad
 """
 
-people = int(input("How many people will eat pizza? "))
-number_of_pizzas = int(input("How many pizzas do you have? "))
-slices_per_pizza = int(input("How many slices are in each pizza? "))
+#step1: get input
+number_people = int(input("Enter the number of people: "))
+total_pizzas = int(input("Enter the total number of pizzas: "))
+slices_per_pizza = int(input("Enter the slices per pizza: "))
 
-total_slices = slices_per_pizza * number_of_pizzas
+#step2: calculate the total slices of pizza
+total_slices = total_pizzas * slices_per_pizza
 
-print(f"There are {people} people and {number_of_pizzas} pizzas.")
+#step3: calucate the slices per person
+slices_per_person = total_slices // number_people
 
-pizza_slices_per_person = total_slices / people
-print(f"Each person gets {pizza_slices_per_person} pieces of pizza.")
+#step4: calculate the leftover pizza
+leftover_pizza = total_slices % number_people
 
-leftover_pieces = total_slices % people
-print(f"There are {int(leftover_pieces)} leftover pieces.")
+#step5: write out how much pizza everyone gets using if-else to account for pluralization
+if slices_per_person == 1:
+    print(f"Each person gets one piece of pizza.")
+else: 
+    print(f"Each person gets {slices_per_person} pieces of pizza.")
 
-
-print("Below is an exercise challenge:")
-
-eaters = int(input("How many people are eating pizza? "))
-number_of_pieces_wanted = int(input("How many pieces of pizza does each person want? "))
-
-total_slices_needed = eaters * number_of_pieces_wanted
-
-slices_in_a_pizza = 12
-
-total_pizzas = total_slices_needed / slices_in_a_pizza
-
-print(f"We'll need {int(total_pizzas)} total pizzas.")
+#step6: write conditional if there is leftover pizza
+if leftover_pizza >= 1:
+    print(f"There are {leftover_pizza} slices of pizza leftover.")
