c2456caf853b79220909c3a9f5e4c1cf5689096b
Amy Haddad <amy.m.haddad@gmail.com>
2018-08-08 18:02:13 -0400

    Update ex_14_ch4.py

--- ex_14_ch4.py
+++ ex_14_ch4.py
@@ -19,7 +19,7 @@ if normalized_state == "wi":
     print(f"You're a Wisonsin resident, and will be charged a tax of {WI_TAX_PERCENT}%.")
     tax_amount = WI_TAX_PERCENT / 100 * order_amount
     total_with_tax = tax_amount + order_amount
-    print("Your order amount before tax is ${:.2f}".format(order_amount)+", but with tax your grand total is ${:.2f}.".format(total_with_tax))
+    print("Your order amount before tax is ${:.2f}".format(order_amount)", but with tax your grand total is ${:.2f}.".format(total_with_tax))
 #step4: set up conditional for users who don't live in WI
 else: 
     print("You're not a Wisconsin resident, so your total is ${:.2f}.".format(order_amount))
