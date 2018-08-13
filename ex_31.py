75a2f1cb71e5a8f9bb56c6bc584961a440e5bf8a
Amy Haddad <amy.m.haddad@gmail.com>
2018-08-13 14:08:31 -0400

    Update ex_31.py

--- ex_31.py
+++ ex_31.py
@@ -6,14 +6,17 @@ Created on Thu May 10 15:34:22 2018
 @author: amyhaddad
 """
 
+# Step 1: write constants
+LOW_INTENSITY = 55
+HIGH_INTENSITY = 95
+INTERVAL = 5
 
-# Step 1: get input from user 
+# Step 2: get input from user 
 age = int(input("Enter your age: "))
 resting_heart_rate = int(input("Enter your resting heart rate: "))
 
-# Step 2: write "for" loop to determine target heart rate based on given intensities (55% to 95%)
-for value in range(55, 100, 5):
+# Step 3: write loop to determine target HR 
+for value in range(LOW_INTENSITY, HIGH_INTENSITY, INTERVAL):
     intensity_as_decimal = value / 100
     target_hr = (((220 - age) - resting_heart_rate) * intensity_as_decimal) + resting_heart_rate
     print(f"At {value}%, your heart rate should be {int(target_hr)}.")
-    
