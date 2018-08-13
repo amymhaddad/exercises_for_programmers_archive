8065d4db27d245e03d28933c6fef496f8128c56a
Amy Haddad <amy.m.haddad@gmail.com>
2018-08-13 14:04:13 -0400

    Update ex_30.py

--- ex_30.py
+++ ex_30.py
@@ -10,8 +10,8 @@ Created on Thu May 10 15:34:22 2018
 number_outer = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 number_inner = number_outer[:]
 
-# Step 2: write "for" loop that iterates through each outer number and completes math operation on each inner number
-for number1 in number_outer:
-    for number2 in number_inner:
-        product = number1 * number2
-        print(f"{number1} * {number2} = {product}")
+# Step 2: write a nested loop
+for number_1 in number_outer:
+    for number_2 in number_inner:
+        product = number_1 * number_2
+        print(f"{number_1} * {number_2} = {product}")
