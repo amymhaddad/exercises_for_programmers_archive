f507051b6c9360e7ba4a2a8cdf44a0f09b7f72e8
Amy Haddad <amy.m.haddad@gmail.com>
2018-08-13 13:52:43 -0400

    Update ex_14_ch4.py

--- ex_14_ch4.py
+++ ex_14_ch4.py
@@ -6,20 +6,20 @@ Created on Thu May 10 15:34:22 2018
 @author: amyhaddad
 """
 
-#step1: state constant
-WI_TAX_PERCENT = 5.5
+# Step 1: set constant
+WI_TAX = 5.5
 
-#step2: get inputs from user on order amount and state
-order_amount = float(input("Enter the order amount:$"))
-state = input("What state do you live in? Enter the state abbreviation (eg, MA): ")
+# Step 2: get input from user
+order_amount = int(input("Enter the order amount:$"))
+state = input("Enter your state (use state abbreviations): ")
 normalized_state = state.lower()
 
-#step3: set up first conditional to see if user lives in WI
+# Step 3: write conditional
 if normalized_state == "wi":
-    print(f"You're a Wisonsin resident, and will be charged a tax of {WI_TAX_PERCENT}%.")
-    tax_amount = WI_TAX_PERCENT / 100 * order_amount
-    total_with_tax = tax_amount + order_amount
-    print("Your order amount before tax is ${:.2f}".format(order_amount)", but with tax your grand total is ${:.2f}.".format(total_with_tax))
-#step4: set up conditional for users who don't live in WI
-else: 
-    print("You're not a Wisconsin resident, so your total is ${:.2f}.".format(order_amount))
+    tax = (WI_TAX / 100) * order_amount
+    print("You're going to pay ${:.2f}".format(tax) + " in tax.")
+    subtotal = order_amount + tax
+    print("Your order total is ${:.2f}.".format(order_amount))
+    print("Your subtotal is ${:.2f}.".format(subtotal))
+else:
+    print(f"Your subtotal is ${order_amount}.")
