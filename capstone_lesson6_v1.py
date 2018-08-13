9eb1dabd55098ec36eaf5ee5d7bf1d9f046edc37
Amy Haddad <amy.m.haddad@gmail.com>
2018-08-13 14:48:12 -0400

    Update capstone_lesson6_v1.py

--- capstone_lesson6_v1.py
+++ capstone_lesson6_v1.py
@@ -6,14 +6,11 @@ Created on Thu May 10 15:34:22 2018
 @author: amyhaddad
 """
 
-#step1: given variable
-total_minutes = 123
+# Step 1: Create constant
+TOTAL_MINUTES = 123
 
-#step2: calculate total hours
-total_hours = total_minutes // 60
+total_hours = TOTAL_MINUTES // 60
 
-#step3: calculate minutes
-leftover_minutes = total_minutes % 60
+leftover_minutes = TOTAL_MINUTES % 60
 
-#step4: print output
 print(f"Hours:\n{total_hours}\nMinutes:\n{leftover_minutes}")
