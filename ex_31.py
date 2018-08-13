f5704a3be21034794690a1897777abc0eccae34f
Amy Haddad <amy.m.haddad@gmail.com>
2018-08-13 13:39:41 -0400

    ex_31.py
    When getting into a fitness program, you may want to figure out your target heart rate so you don’t overexert yourself. The Karvonen heart rate formula is one method you can use to determine your rate. Create a program that prompts for your age and your resting heart rate. Use the Karvonen for- mula to determine the target heart rate based on a range of intensities from 55% to 95%. Generate a table with the results as shown in the example output. 
    
    The formula is
    TargetHeartRate = (((220 − age) − restingHR) × intensity) + restingHR

---
+++ ex_31.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 10 15:34:22 2018
+
+@author: amyhaddad
+"""
+
+# Step 1: get input from user 
+age = int(input("Enter your age: "))
+resting_heart_rate = int(input("Enter your resting heart rate: "))
+
+# Step 2: write "for" loop to determine target heart rate based on given intensities (55% to 95%)
+for value in range(55, 100, 5):
+    intensity_as_decimal = value / 100
+    target_heart_rate = (((220 - age) - resting_heart_rate) * intensity_as_decimal) + resting_heart_rate
+    print(f"At {value}%, your heart rate should be {int(target_heart_rate)}.")
