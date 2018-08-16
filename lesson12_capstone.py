e896e9cd832745ae1f89ea2396da7552d7dfc0fc
Amy Haddad <amy.m.haddad@gmail.com>
2018-08-16 14:44:52 -0400

    Update lesson12_capstone.py

--- lesson12_capstone.py
+++ lesson12_capstone.py
@@ -6,53 +6,38 @@ Created on Thu May 10 15:34:22 2018
 @author: amyhaddad
 """
 
-#step1: get input from user for two first and last names
-full_name_1 = input("Enter one full name in the format FIRST LAST: ")
+# Step 1: get input for two names 
+name_1 = input("Enter a name (first and last name): ")
+name_2 = input("Enter a second name (first and last name): ")
 
-full_name_2 = input("Enter a second full name in the format FIRST LAST: ")
+# Step 2: separate the names
+space1 = name_1.find(' ')
+space2 = name_2.find(' ')
 
-#step2: create variables to separate each first name and each last name
+first_name_name_1 = name_1[:space1]
+first_name_name_2 = name_2[:space2]
+last_name_name_1 = name_1[space1+1:]
+last_name_name_2 = name_2[space2+1:]
 
-space1 = full_name_1.find(" ")
-first_firstname = full_name_1[0:space1]
-first_lastname = full_name_1[space1 + 1:]
+# Step 3: divide each first name in half and then divide each last name in half
+right_half_first_name_name_1 = first_name_name_1[2:]
+left_half_first_name_name_1 = first_name_name_1[:2]
+right_half_first_name_name_2 = first_name_name_2[2:]
+left_half_first_name_name_2 = first_name_name_2[:2]
 
-space2 = full_name_2.find(" ")
-second_firstname = full_name_2[0:space2]
-second_lastname = full_name_2[space2+1:]
+right_half_last_name_name_1 = last_name_name_1[2:]
+left_half_last_name_name_1 = last_name_name_1[:2]
+right_half_last_name_name_2 = last_name_name_2[2:]
+left_half_last_name_name_2 = last_name_name_2[:2]
 
-#step3: write a line of code to change the first letter of each name using index and use .replace
-first_letter_firstname_name_1 = first_firstname[0]
-first_letter_lastname_name_1 = first_lastname[0]
+# Step 4: combine the names to get new names
+new_first_name_name_1 = right_half_first_name_name_1 + left_half_first_name_name_2
+new_first_name_name_2 = right_half_first_name_name_2 + left_half_first_name_name_1
+new_last_name_name_1 = right_half_last_name_name_1 + left_half_last_name_name_2
+new_last_name_name_2 = right_half_last_name_name_2 + left_half_last_name_name_1
 
-first_letter_firstname_name_2 = second_firstname[0]
-first_letter_lastname_name_2 = second_lastname[0]
+# Step 5: print output
+full_new_name_name_1 = (f"{new_first_name_name_1.capitalize()} {new_last_name_name_1.capitalize()}")
+full_new_name_name_2 = (f"{new_first_name_name_2.capitalize()} {new_last_name_name_2.capitalize()}")
 
-new_firstname = first_firstname.replace(first_letter_firstname_name_1, first_letter_firstname_name_2)
-new_lastname = first_lastname.replace(first_letter_lastname_name_1, first_letter_lastname_name_2)
-print(f"Here's the first new name option: {new_firstname} {new_lastname}")
-
-#step4: write a line of code to swap the names using a tuple
-swap_firstname = (first_firstname, second_firstname) = (second_firstname, first_firstname)
-swap_lastname = (first_lastname, second_lastname) = (second_lastname, first_lastname)
-
-#step4a: add the truples together so all objects are within one truple
-swapped_name = swap_firstname + swap_lastname
-
-#step4b: index the newly formed truple to form new first and last names for one name
-swapped_new_first_name1 = swapped_name[0]
-swapped_new_last_name1 = swapped_name[3]
-swapped_first_and_lastname_v1 = (swapped_new_first_name1, swapped_new_last_name1)
-
-swapped_new_firstname_name1 = swapped_first_and_lastname_v1[0]
-new_lastname_name1 = swapped_first_and_lastname_v1[1]
-
-#step4c: index the newly formed truple to form new first and last names for second name
-swapped_new_first_name2 = swapped_name[1]
-swapped_new_last_name2 = swapped_name[2]
-swapped_first_and_lastname_v2 = (swapped_new_first_name2, swapped_new_last_name2)
-
-swapped_new_firstname_name2 = swapped_first_and_lastname_v2[0]
-swapped_new_lastname_name2 = swapped_first_and_lastname_v2[1]
-
-print(f"Here's the second new name option: {swapped_new_firstname_name2} {swapped_new_lastname_name2}.")
+print(f"These are the new names: {full_new_name_name_1} and {full_new_name_name_2}")
