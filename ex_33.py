bb56bd346baa0f0858ae4943e96f62ffdfd6392b
Amy Haddad <amy.m.haddad@gmail.com>
2018-08-20 15:09:01 -0400

    Update ex_33.py

--- ex_33.py
+++ ex_33.py
@@ -12,7 +12,7 @@ import random
 # Step 2: create a list of answer possibilities
 answers = ['Yes', 'No', 'Maybe', 'Ask again later']
 
-# Step 3: write a loop 
+# Step 3: write a loop
 for question in input("What's your question? "):
     random_number = random.randint(0, len(answers)-1)
     magic_eight_response = answers[random_number]
