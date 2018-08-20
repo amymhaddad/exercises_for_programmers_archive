675f463cfd388d15552c504abb823da3e9a4c94a
Amy Haddad <amy.m.haddad@gmail.com>
2018-08-20 17:31:15 -0400

    Update ex_34.py

--- ex_34.py
+++ ex_34.py
@@ -13,20 +13,22 @@ total_employees = len(employee_names)
 
 # Step 2: print the total number of employees and print their names
 print(f"There are {total_employees} employees: ")
+
 for name in employee_names:
     print(name.title())
 
 print("\n")
 
 # Step 3: get user input on employee removal and remove employee
-employee_removal = input("Which employee should I remove? ")
-normalized_employee_removal = employee_removal.lower()
+employee_to_remove = input("Which employee should I remove? ")
+normalized_employee_to_remove = employee_to_remove.lower()
 
-employee_names.remove(normalized_employee_removal)
+employee_names.remove(normalized_employee_to_remove)
 
 print("\n")
 
 # Step 5: print the remaining employees
 print(f"There are {total_employees} employees remaining: ")
+
 for name in employee_names:
     print(name.title())
