2465eefca11bd3ba6a1ddedc10487fe8cb1b2e2d
Amy Haddad <amy.m.haddad@gmail.com>
2018-09-10 08:00:55 -0400

    ex_24.py
    Create a program that compares two strings and determines if the two strings are anagrams. The program should prompt for both input strings and display the output as shown in the example that follows:
    Enter two strings and I'll tell you if they are anagrams:
           Enter the first string: note
           Enter the second string: tone
           "note" and "tone" are anagrams.
    
    Constraints:
    Implement the program using a function called isAna- gram, which takes in two words as its arguments and returns true or false. Invoke this function from your main program.

---
+++ ex_24.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 10 15:34:22 2018
+
+@author: amyhaddad
+"""
+
+# Step 1: print directions
+print("Enter two strings and I'll tell you if they are anagrams:")
+
+# Step 2: get input from user on two words
+user_string_1 = input("Enter string one: ")
+user_string_2 = input("Enter string two: ")
+
+# Step 3: write a function to see if the words are anagrams
+def is_anagram (word_1, word_2):
+    """Compare two strings to determine if they are anagrams"""
+    word_1_length = len(word_1)
+    word_2_length = len(word_2)
+
+    if word_1_length == word_2_length:
+        sorted_word_1 = sorted(word_1)
+        sorted_word_2 = sorted(word_2)
+        if sorted_word_1 == sorted_word_2:
+            print(f"\'{word_1}\' and \'{word_2}\' are anagrams.")
+    else:
+        print(f"\'{word_1}\' and \'{word_2}\' are not anagrams.")
+
+user_input = is_anagram(user_string_1, user_string_2)
