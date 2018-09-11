2486f33a5f7e07d25eda856c7b1f0f0d592821ec
Amy Haddad <amy.m.haddad@gmail.com>
2018-09-11 16:39:08 -0400

    Update ex_35.py

--- ex_35.py
+++ ex_35.py
@@ -6,22 +6,17 @@ Created on Thu May 10 15:34:22 2018
 @author: amyhaddad
 """
 
-# Step 1: import random and create empty list
 import random
 
 contestants = []
 
-# Step 2: write while loop
-contestant_prompt = "Enter a name: "
-
-active = True
-while active:
-    message = input(contestant_prompt)
-
-    if message == ' ':
-        random_contestant = random.randint(0, len(contestants)-1)
-        select_winner = contestants[random_contestant]
-        print(f"The winner is {select_winner.capitalize()}.")
-        active = False
+while True:
+    contestant_name = input("Enter a name: ")
+    if contestant_name == ' ':
+        random_winner = random.randint(0, len(contestants)-1)
+        winner = contestants[random_winner]
+        print(f"The winner is {winner.title()}.")
+        break
     else:
-        contestants.append(message)
+        contestants.append(contestant_name)
+        
