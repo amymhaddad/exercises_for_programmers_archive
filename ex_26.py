3bb5d34df582ba08f87542e499854c121f66530b
Amy Haddad <amy.m.haddad@gmail.com>
2018-09-14 14:48:22 -0400

    Update ex_26.py

--- ex_26.py
+++ ex_26.py
@@ -6,7 +6,7 @@ Created on Thu May 10 15:34:22 2018
 @author: amyhaddad
 """
 
-import math 
+import math
 
 # Get input from user
 credit_card_balance = int(input("Enter your credit card balance: $"))
@@ -16,10 +16,12 @@ apr_as_decimal = apr / 100
 apr_daily_rate = apr_as_decimal / 365
 
 def calc_months_until_paid_off(credit_card_balance, apr, monthly_payment):
+    
+    
     """Write a function to determine the number of months until credit card is paid off"""
 
     number_of_months_to_pay_cc = (-(1 / 30) * \
-                                math.log(1 + credit_card_balance / monthly_payment * (1 - (1 + apr_daily_rate) ** 30)) / math.log(1 + apr_daily_rate))
+                                  math.log(1 + credit_card_balance / monthly_payment * (1 - (1 + apr_daily_rate) ** 30)) / math.log(1 + apr_daily_rate))
     
     print("It will take you {:.2f}".format(number_of_months_to_pay_cc) + " months to pay off your credit card.")
 
